acdf3d3635a5df5b0b12d61d637a9ef0a7904ceb
MrRojito-Dev <cancinojuaco@gmail.com>
2021-07-28 16:17:51 -0400

    messageReactionRemove (autoroles)

---
+++ src/eventos/messageReactionRemove.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js-light");
+module.exports = {
+    name: "messageReactionRemove",
+    emitter: "on",
+    run: async (client, messageReaction, user) => {
+        // Reaction roles
+        if (messageReaction.message.id !== "870032853545664553") return;
+
+        const member = await messageReaction.message.guild.members.fetch(user.id);
+
+        const PiñaFan = messageReaction.message.guild.roles.cache.get("869968063867518997");
+        const RojiFan = messageReaction.message.guild.roles.cache.get("860952790602874900");
+
+        switch (messageReaction.emoji.name) {
+            case "🪀":
+                if (member.roles.cache.has(RojiFan)) {
+                    member.roles.remove(RojiFan);
+                }
+                break;
+    
+            case "🍍":
+                if (member.roles.cache.has(PiñaFan)) {
+                    member.roles.remove(PiñaFan);
+                }
+                break;
+
+            default:
+                break;
+        };
+}}
